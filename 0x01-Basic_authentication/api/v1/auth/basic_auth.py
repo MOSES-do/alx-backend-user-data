a66480add5ab0cc3f9f34e461b32077fa7274915
MOSES-do <archer.me24@gmail.com>
2024-09-06 03:24:12 +0000

    User credential validation

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,10 @@
 import re
 import base64
 import binascii
+from typing import TypeVar
+
 from .auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -49,3 +52,17 @@ class BasicAuth(Auth):
                     decoded_base64_authorization_header.split(':', 1)
             return username, password
         return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Validate user credential submission in database"""
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+                if len(users) <= 0:
+                    return None
+                if users[0].is_valid_password(user_pwd):
+                    return users[0]
+        return None
