e9f36abc56a5fce6b050036fde54a0709015c2f7
MOSES-do <archer.me24@gmail.com>
2024-09-06 02:45:25 +0000

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 """Basic Authentication module API"""
 import re
+import base64
+import binascii
 from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic Auth BluePrint"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """Extracts the Base64 part of the Authorization header
         for a Basic Authentication.
         """
         if type(authorization_header) == str:
+            """regex exp checking for keyword 'Basic' and other space
+                separated value(capture group)"""
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """Decode base64 encoded auth header """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
