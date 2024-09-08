305b88048d2ef2b860b963ef730c336ded632a91
MOSES-do <archer.me24@gmail.com>
2024-09-08 02:59:10 +0000

    Creating session_id for user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """Create SessionAuth Flow"""
 
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session Auth"""
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if type(user_id) == str:
+             session_id = str(uuid.uuid4())
+             self.user_id_by_session_id[session_id] = user_id
+             return session_id
+        return None
